02531dff809b6b601042847b21491790d07888d4
elnardu <elnardu2@gmail.com>
2021-08-05 09:42:57 -0700

    Configuration for discord logging

---
+++ fireball/logging.py
@@ -0,0 +1,34 @@
+import logging
+import asyncio
+from typing import Dict, Any
+
+import aiohttp
+
+
+class DiscordHandler(logging.Handler):
+    def __init__(self, webhook_url: str):
+        super(DiscordHandler, self).__init__()
+        self._webhook_url = webhook_url
+        self._session = aiohttp.ClientSession()
+
+    def emit(self, record: logging.LogRecord):
+        data = self._format_message(record)
+
+        # TODO: not really sure what to do with this task thing,
+        # since I cannot await it here
+        asyncio.create_task(self.send_message(data))
+
+    def _format_message(self, record: logging.LogRecord) -> Dict[str, Any]:
+        return {"content": record.getMessage()}
+
+    async def send_message(self, data: Dict[str, Any]):
+        await self._session.post(self._webhook_url, json=data)
+
+
+def configure_discord_logging(webhook_url: str):
+    root = logging.getLogger()
+
+    discord_handler = DiscordHandler(webhook_url)
+    discord_handler.setLevel(logging.INFO)
+
+    root.addHandler(discord_handler)
